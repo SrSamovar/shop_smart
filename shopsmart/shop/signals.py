773a12b491b8971c4d742cca91df0460a26a3bab
SrSamovar <levchenkovadim2001@mail.ru>
2025-02-13 21:03:03 +0300

    add register signal

--- shopsmart/shop/signals.py
+++ shopsmart/shop/signals.py
@@ -1,12 +1,18 @@
+from typing import Type
+
 from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
+from django.db.models.signals import post_save
 from django.dispatch import Signal, receiver
 from django.template.defaultfilters import title
 
-from .models import User
+from .models import User, EmailToken
 
 
 new_order = Signal()
+
+
+
 
 
 @receiver(new_order)
@@ -22,3 +28,25 @@ def new_order_signal(user_id, **kwargs):
         [user.email],
     )
     msg.send()
+
+
+@receiver(post_save, sender=User)
+def new_user_registered_signal(sender: Type[User], instance: User, created: bool, **kwargs):
+    """
+     отправляем письмо с подтрердждением почты
+    """
+    if created and not instance.is_active:
+        # send an e-mail to the user
+        token, _ = EmailToken.objects.get_or_create(user_id=instance.pk)
+
+        msg = EmailMultiAlternatives(
+            # title:
+            f"Password Reset Token for {instance.email}",
+            # message:
+            token.key,
+            # from:
+            settings.EMAIL_HOST_USER,
+            # to:
+            [instance.email]
+        )
+        msg.send()
